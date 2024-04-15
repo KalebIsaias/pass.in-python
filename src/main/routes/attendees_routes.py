7f36ee718a52299fed5606228d1d964ec3234cb1
KalebIsaias <kiscstudies@gmail.com>
2024-04-15 15:07:00 -0300

    feat: implementa a rota dos attendees pelo event_id

--- src/main/routes/attendees_routes.py
+++ src/main/routes/attendees_routes.py
@@ -25,3 +25,13 @@ def find_attendee_badge(attendee_id: str):
 
   response = attendees_handler.find_attendee_badge(http_request)
   return jsonify(response.body), response.status_code
+
+@attendees_route_bp.route('/events/<event_id>/attendees', methods=['GET'])
+def get_attendees(event_id: str):
+  attendees_handler = AttendeesHandler()
+  http_request = HttpRequest(
+    param={ "event_id": event_id },
+  )
+
+  response = attendees_handler.find_attendees_from_event(http_request)
+  return jsonify(response.body), response.status_code
