a7308d084e0ebc0d026bceb41f759d6057363389
KalebIsaias <kiscstudies@gmail.com>
2024-04-15 11:28:32 -0300

    feat: adiciona a logica para criar um evento

--- src/main/routes/events_routes.py
+++ src/main/routes/events_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from src.http_types.http_request import HttpRequest
+from src.data.event_handler import EventHandler
 
 event_route_bp = Blueprint('event_route', __name__)
 
@@ -8,4 +9,7 @@ def create_event():
   http_request = HttpRequest(
     body=request.json,
   )
-  return jsonify({ 'ola': 'mundo' }), 200
+  event_handler = EventHandler()
+
+  response = event_handler.register(http_request)
+  return jsonify(response.body), response.status_code
